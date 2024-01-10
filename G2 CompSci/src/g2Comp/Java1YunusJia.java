61af330c751192bca0b13d85db5f3d6465910cc6
Yunnus Jia <110269012+0xYunnis@users.noreply.github.com>
2024-01-10 19:49:57 +0800

    Add files via upload

---
+++ G2 CompSci/src/g2Comp/Java1YunusJia.java
@@ -0,0 +1,78 @@
+package g2Comp;
+
+public class Java1YunusJia {
+  public static void main(String[] args) {
+	  System.out.println("Hello");
+	  System.out.println("World");
+	  System.out.println("From");
+	  System.out.println("YunusJia");
+  }
+}
+
+/*package g2Comp;
+
+public class Java2YunusJia {
+	public static void main(String[] args) {
+		//task3
+		String st1="bang";
+		String st2="me";
+		System.out.println(st1+st2);
+		//int mathmatics test
+int ans=0;
+int a=1,b=2;
+ans=a+b;
+System.out.println(ans);
+ans=b-a;
+System.out.println(ans);
+ans=b*a;
+System.out.println(ans);
+ans=b/a;
+System.out.println(ans);
+double ansd=0;
+double as=2.987,ab=2712312313.99999;
+ansd=as+ab;
+System.out.println(ansd);
+ansd=as-ab;
+System.out.println(ansd);
+ansd=as*ab;
+System.out.println(ansd);
+ansd=as/ab;
+System.out.println(ansd);
+
+	      
+}
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ G2 CompSci/src/g2Comp/Java2YunusJia.java
@@ -0,0 +1,111 @@
+
+package g2Comp;
+import java.util.Scanner;
+import java.util.Random;
+public class Java2YunusJia {
+  public static void main(String[] args) {
+	  //task1
+     /* System.out.println ("enter something");
+	  Scanner bdscans=new Scanner(System.in);
+	  String a=bdscans.next();
+	  System.out.println("hi"+a);
+
+	  
+	  //task2 
+	  Scanner Kbtype=new Scanner(System.in);
+		int Num1=Kbtype.nextInt();
+		Scanner MMtype=new Scanner(System.in);
+		int Num2=MMtype.nextInt();
+		int Ans= Num1*Num2;
+		System.out.println (Ans);
+		System.out.println (Num1);
+		System.out.println (Num2);
+		
+		Scanner RAL=new Scanner(System.in);
+	      double NNUM1=RAL.nextDouble();
+	      Scanner RAL1=new Scanner(System.in);
+	      double NNUM2=RAL1.nextDouble();
+	      int Ans2= Num1*Num2;
+	      System.out.print(RAL);
+	      System.out.print(RAL1);
+	      System.out.print(Ans2);  
+	    
+	   //task3
+	      int num=10;
+	      int num1=3;
+	      //DIV
+	        int asn=num/num1;
+	        int asn1=Math.floorDiv(num,num1);
+	        System.out.println(asn);
+	        System.out.println(asn1);
+	      //MOD
+	        int asn2=num%num1;
+	        int asn3=Math.floorMod(num,num1);
+	        System.out.println(asn2);
+	        System.out.println(asn3);
+	      //ROUND
+	        System.out.println("please enter a real number");
+	        Scanner g=new Scanner(System.in);
+	        double num2=g.nextDouble();
+	        double asn4=Math.round(num2);
+	        System.out.println(asn4);
+	        */
+	     //RANDOM
+	  for(int i=0;i<=2;i++) {
+	        System.out.println( Math.random()*10);
+	       
+	        
+	        
+	    		  
+	      
+	  }
+	  int counter=0;
+	  while(counter<=2) {
+		  Random rand=new Random();
+		  double RanNum=rand.nextInt();
+		  System.out.println(RanNum*10);
+		  counter++;
+		  
+			
+	  }
+	     
+	  
+  }
+}
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
